53e9955c325620af882d898399fc7ddbcdb6da81
Ömer Faruk MENDİ <omerfarukmendi@gmail.com>
2020-07-13 20:46:02 +0000

    Create user model

---
+++ api/user/models.py
@@ -0,0 +1,8 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
