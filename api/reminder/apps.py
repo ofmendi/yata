1cfb59b67792d629cbb35d027fb2c69c72287e9d
Ömer Faruk MENDİ <omerfarukmendi@gmail.com>
2020-07-14 19:58:48 +0000

    Start scheduler when reminder loaded

--- api/reminder/apps.py
+++ api/reminder/apps.py
@@ -4,3 +4,8 @@ from django.conf import settings
 
 class ReminderConfig(AppConfig):
     name = 'reminder'
+
+    def ready(self):
+        from . import scheduler
+        if settings.SCHEDULER_AUTOSTART:
+        	scheduler.start()
