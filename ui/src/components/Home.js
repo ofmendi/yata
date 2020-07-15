5c388e6297e86105575b8564fd8f665e6e35d39c
Ömer Faruk MENDİ <omerfarukmendi@gmail.com>
2020-07-15 20:16:11 +0000

    Create Home component

---
+++ ui/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="container">
+        <h4 className="center">Welcome to Yep Another Todo App</h4>
+        <p>In this application, your mission is to find the system vulnerability with usual vulnerability research methodologies. When you found it, you'll able to exploit the system.</p>
+        <p>If you'd like to know why you are researching a todo app, the reason is when software developers learn a new programming language or a new framework, they often develop a todo application which helps to understand the language or the framework better. Therefore maybe this application helps you to understand basic vulnerability researches methodologies.</p>
+        <p>Who knows maybe this application will be your first step in your vulnerability researcher career.</p>
+        <p> I hope you'll enjoy it and good luck.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
