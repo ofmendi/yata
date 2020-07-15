13b29df7a6701d6eba85a6b2ab08b58006375e65
Ömer Faruk MENDİ <omerfarukmendi@gmail.com>
2020-07-15 20:20:55 +0000

    Create react app

---
+++ ui/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Router from "./components/Router";
+import '@progress/kendo-theme-default/dist/all.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router />
+    );
+  }
+}
+
+export default App;
